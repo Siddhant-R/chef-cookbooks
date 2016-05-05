e04a436c479152fc0c7c81e6507a3bb15f06eb7e
Siddhant Rath <sid@tamu.edu>
2016-05-05 15:55:01 -0500

    Set SELinux to permissive mode

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,9 @@
 name 'awesome_customers_rhel'
 maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer_email 'sid@tamu.edu'
 license 'all_rights'
 description 'Installs/Configures awesome_customers_rhel'
 long_description 'Installs/Configures awesome_customers_rhel'
 version '0.1.0'
+
+depends 'selinux', '~> 0.9.0'
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,7 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+#
+
+#Set SELINUX to permissive mode
+include_recipe 'selinux::permissive'
