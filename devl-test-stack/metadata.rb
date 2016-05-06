eb8279f6c8adb63055237d7115e2028c8f8c10b0
Siddhant Rath <sid@tamu.edu>
2016-05-06 17:29:12 -0500

    devel-test-stack:denyhosts:thr complete

--- devl-test-stack/metadata.rb
+++ devl-test-stack/metadata.rb
@@ -1,7 +1,11 @@
 name 'devl-test-stack'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'CSE TAMU'
+maintainer_email 'sid@tamu.edu'
 license 'all_rights'
 description 'Installs/Configures devl-test-stack'
 long_description 'Installs/Configures devl-test-stack'
 version '0.1.0'
+
+#Deny-Hosts
+depends "yum-epel"
+
