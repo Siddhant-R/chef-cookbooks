68e0184cd1dc45354d971748d9458be81481d632
Siddhant Rath <sid@tamu.edu>
2016-05-06 14:49:30 -0500

    Configure the database

--- attributes/default.rb
+++ attributes/default.rb
@@ -14,3 +14,9 @@ default['awesome_customers_rhel']['document_root'] = '/var/www/customers/public_
 
 default_unless['awesome_customers_rhel']['database']['root_password'] = 'mysql_root_password'
 default_unless['awesome_customers_rhel']['database']['admin_password'] = random_password
+
+default['awesome_customers_rhel']['database']['dbname'] = 'my_company'
+default['awesome_customers_rhel']['database']['host'] = '127.0.0.1'
+default['awesome_customers_rhel']['database']['root_username'] = 'root'
+default['awesome_customers_rhel']['database']['admin_username'] = 'db_admin'
+
